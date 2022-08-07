8436627a052ce79ceea711cd92ca4bea481ab5c8
Dovile-dote <dbilkstiene@gmail.com>
2022-08-07 10:36:56 +0300

    new fotos

--- duomenys/slaiderioDuomenys.js
+++ duomenys/slaiderioDuomenys.js
@@ -1,43 +1,42 @@
 const slaiderioDuomenys = {
-    srcFolder: './nuotraukos/',
-    list: [
-        {
-            src: '1-foto.jpg',
-            alt: 'Pirma nuotrauka',
-            title: 'Elektra',
-            years: '2016m.',
-            format: '50x60cm',
-        },
-        {
-            src: '2-foto.jpg',
-            alt: 'Antra nuotrauka',
-            title: 'Grybas',
-            years: '2013m.',
-            format: '70x70cm',
-        },
-        {
-            src: '3-foto.jpg',
-            alt: 'Trecia nuotrauka',
-            title: 'Drugeliai',
-            years: '2018m.',
-            format: '50x60cm',
-        },
-        {
-            src: '4-foto.jpg',
-            alt: 'Ketvirta nuotrauka',
-            title: 'Namas',
-            years: '2014m.',
-            format: '70x60cm',
-        }, 
-        {
-            src: '5-foto.jpg',
-            alt: 'Penkta nuotrauka',
-            title: 'Kompozitorius',
-            years: '2017m.',
-            format: '90x70cm',
-        },
-    ]
+  srcFolder: './nuotraukos/',
+  list: [
+    {
+      src: '1-foto.jpg',
+      alt: 'Pirma nuotrauka',
+      title: 'Tigras',
+      years: '2018m.',
+      format: '60x70cm',
+    },
+    {
+      src: '2-foto.jpg',
+      alt: 'Antra nuotrauka',
+      title: 'Grybas',
+      years: '2013m.',
+      format: '70x70cm',
+    },
+    {
+      src: '3-foto.jpg',
+      alt: 'Trecia nuotrauka',
+      title: 'Drugeliai',
+      years: '2018m.',
+      format: '50x60cm',
+    },
+    {
+      src: '4-foto.jpg',
+      alt: 'Ketvirta nuotrauka',
+      title: 'Namas',
+      years: '2014m.',
+      format: '70x60cm',
+    },
+    {
+      src: '5-foto.jpg',
+      alt: 'Penkta nuotrauka',
+      title: 'Kompozitorius',
+      years: '2017m.',
+      format: '90x70cm',
+    },
+  ],
 };
 
-
-export { slaiderioDuomenys }
+export { slaiderioDuomenys };
